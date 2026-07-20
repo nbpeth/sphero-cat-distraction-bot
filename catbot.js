202ada58c2d0ea7e15400ad5cc13793719e22959
Nicholas Peth <nicholaspeth@Mac.lan>
2026-07-20 11:42:36 -0600

    formatting

--- catbot.js
+++ catbot.js
@@ -1,4 +1,3 @@
-
 const MAX_SPEED = 255;
 const MIN_SPEED = 1;
 const CRUISE_SPEED = 75;
@@ -25,145 +24,142 @@ let index = 0;
 // https://sites.google.com/sphero.com/sphero-edu-javascript-wiki
 
 const colors = {
-    "brightRed": { r: 255, g: 0, b: 0 },
-    "darkRed": { r: 100, g: 0, b: 0 },
-    "brightGreen": { r: 0, g: 255, b: 0 },
-    "brightBlue": { r: 0, g: 0, b: 255 },
-    "darkBlue": { r: 0, g: 0, b: 255 },
-    "cyan": { r: 0, g: 255, b: 255 },
-    "lime": { r: 255, g: 255, b: 0 },
-    "brightWhite": { r: 255, g: 255, b: 255 },
-}
+  brightRed: { r: 255, g: 0, b: 0 },
+  darkRed: { r: 100, g: 0, b: 0 },
+  brightGreen: { r: 0, g: 255, b: 0 },
+  brightBlue: { r: 0, g: 0, b: 255 },
+  darkBlue: { r: 0, g: 0, b: 255 },
+  cyan: { r: 0, g: 255, b: 255 },
+  lime: { r: 255, g: 255, b: 0 },
+  brightWhite: { r: 255, g: 255, b: 255 },
+};
 
 const colorsAsList = Object.values(colors);
 
 const getTotalVelocity = () => {
-    return Math.sqrt((getVelocity().x ** 2) + (getVelocity().y ** 2))
-}
+  return Math.sqrt(getVelocity().x ** 2 + getVelocity().y ** 2);
+};
 
 const setNewHeading = (angleAdjust) => {
-    let newHeading = (getHeading() - angleAdjust + FULL_CIRCLE_DEGREES) % FULL_CIRCLE_DEGREES;
-    setHeading(newHeading);
+  let newHeading =
+    (getHeading() - angleAdjust + FULL_CIRCLE_DEGREES) % FULL_CIRCLE_DEGREES;
+  setHeading(newHeading);
 
-    return newHeading;
-}
+  return newHeading;
+};
 
 const randomNumber = (floor, ceiling) => {
-    return Math.floor(Math.random() * ceiling) + floor;
-}
+  return Math.floor(Math.random() * ceiling) + floor;
+};
 
 const turnLeft = () => {
-    setFrontLed(colors.darkRed)
-    setBackLed(colors.darkBlue)
-    setNewHeading(LEFT_TURN_ANGLE)
-}
+  setFrontLed(colors.darkRed);
+  setBackLed(colors.darkBlue);
+  setNewHeading(LEFT_TURN_ANGLE);
+};
 
 const turnRight = () => {
-    setFrontLed(colors.darkBlue);
-    setBackLed(colors.darkRed);
+  setFrontLed(colors.darkBlue);
+  setBackLed(colors.darkRed);
 
-    setNewHeading(RIGHT_TURN_ANGLE);
-}
+  setNewHeading(RIGHT_TURN_ANGLE);
+};
 
 const reverse = () => {
-    setFrontLed(colors.lime);
-    setBackLed(colors.cyan);
+  setFrontLed(colors.lime);
+  setBackLed(colors.cyan);
 
-    setNewHeading(REVERSE_ANGLE);
-}
+  setNewHeading(REVERSE_ANGLE);
+};
 
 const turnLeftOrRight = async () => {
-    const rando = randomNumber(0, DIRECTION_CHOICE_COUNT);
+  const rando = randomNumber(0, DIRECTION_CHOICE_COUNT);
 
-    if (rando % 3 === 0) {
-        return turnLeft();
-    } else if (rando % 2 === 0) {
-        return turnRight();
-    } else if (rando % 1 === 0) {
-        return reverse();
-    }
-
+  if (rando % 3 === 0) {
+    return turnLeft();
+  } else if (rando % 2 === 0) {
     return turnRight();
-}
+  } else if (rando % 1 === 0) {
+    return reverse();
+  }
 
-
+  return turnRight();
+};
 
 async function onCollision() {
-    // await speak("ouch!");
-    setIndex();
-    setMainLed(colors.darkRed);
+  // await speak("ouch!");
+  setIndex();
+  setMainLed(colors.darkRed);
 
-    turnLeftOrRight();
+  turnLeftOrRight();
 
-    await roll(getHeading(), MAX_SPEED)
+  await roll(getHeading(), MAX_SPEED);
 }
 
 registerEvent(EventType.onCollision, onCollision);
 
 function setIndex() {
-    index++;
-    if (index > colors.length - 1) {
-        index = 0;
-    }
-    return index;
+  index++;
+  if (index > colors.length - 1) {
+    index = 0;
+  }
+  return index;
 }
 
 const unStick = async () => {
+  setMainLed(colors.brightGreen);
 
-    setMainLed(colors.brightGreen);
-    turnLeft();
-
-    await roll(getHeading(), MAX_SPEED)
-}
+  turnLeft();
+  await roll(getHeading(), MAX_SPEED);
+};
 
 async function checkIfStuck() {
-    let totalVelocity = getTotalVelocity();
+  let totalVelocity = getTotalVelocity();
 
-    if (totalVelocity < STUCK_VELOCITY_THRESHOLD) {
-        await unStick();
-    }
+  if (totalVelocity < STUCK_VELOCITY_THRESHOLD) {
+    await unStick();
+  }
 }
 
 async function sprint(time) {
-    await roll(getHeading(), MAX_SPEED, time)
+  await roll(getHeading(), MAX_SPEED, time);
 }
 
 async function rollWithVariableSpeed() {
-    let random = randomNumber(0, SPRINT_ODDS);
-    let shouldSprint = random % SPRINT_ODDS === 0;
+  let random = randomNumber(0, SPRINT_ODDS);
+  let shouldSprint = random % SPRINT_ODDS === 0;
 
-    if (shouldSprint) {
-        await sprint(VARIABLE_SPRINT_DURATION_SECONDS)
-    } else {
-        await roll(getHeading(), CRUISE_SPEED)
-    }
+  if (shouldSprint) {
+    await sprint(VARIABLE_SPRINT_DURATION_SECONDS);
+  } else {
+    await roll(getHeading(), CRUISE_SPEED);
+  }
 }
 
 async function chaseMeProtocol() {
-    setMainLed(colors.brightWhite);
-        setIndex();
+  setMainLed(colors.brightWhite);
+  setIndex();
 
-        let random = randomNumber(0, TAUNT_ODDS);
-        if (random % TAUNT_ODDS === 0) {
-            await stopRoll();
-            setMainLed(colors.brightGreen);
-            await delay(TAUNT_PAUSE_DELAY_SECONDS);
-            await resetAim();
-            await sprint(TAUNT_SPRINT_DURATION_SECONDS);
-        } else {
-            await rollWithVariableSpeed();
-        }
+  let random = randomNumber(0, TAUNT_ODDS);
+  if (random % TAUNT_ODDS === 0) {
+    await stopRoll();
+    setMainLed(colors.brightGreen);
+    await delay(TAUNT_PAUSE_DELAY_SECONDS);
+    await resetAim();
+    await sprint(TAUNT_SPRINT_DURATION_SECONDS);
+  } else {
+    await rollWithVariableSpeed();
+  }
 
+  checkIfStuck();
 
-        checkIfStuck();
-
-        await delay(CYCLE_DELAY_SECONDS);
+  await delay(CYCLE_DELAY_SECONDS);
 }
 
 async function startProgram() {
-    setMainLed(colors.brightWhite);
+  setMainLed(colors.brightWhite);
 
-    while (true) {
-        chaseMeProtocol();
-    }
+  while (true) {
+    chaseMeProtocol();
+  }
 }
